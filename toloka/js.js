79e081d8692c79f1715709fbdc85588efd352b8e
Artyom Grigoriev <ortemij@yandex-team.ru>
2021-02-19 19:22:36 +0300

    Task UI updates

--- toloka/js.js
+++ toloka/js.js
@@ -5,44 +5,47 @@ exports.Task = extend(TolokaHandlebarsTask, function (options) {
     TolokaHandlebarsTask.call(this, options);
   }, {
     onRender: function() {
-      var inputKey = this.getTask().input_values.input_key;
-      var assignment = this.getAssignment();
-      var assignmentId = assignment.getId();
+      const inputKey = this.getTask().input_values.input_key;
+      const assignment = this.getAssignment();
+      const assignmentId = assignment.getId();
   
       // Change URL of image content in the task to data storage backend
       // Pass assignmentId and input_key to check the access (see /index.js)
       $(this.getDOMElement()).find('img.data').attr('src', `${deployUrl}/data/${inputKey}?assignmentId=${assignmentId}`);
   
       // Attach on-click listener
-      (function (task) {
-        $(task.getDOMElement()).find('.submit').click(function (e) {
-          // Get value from the input field
-          var secret = $(task.getDOMElement()).find('.secret').val();
-  
-          // Call data storage backend
-          $.ajax(
-            {
-              url: `${deployUrl}/result`,
-              type: 'PUT',
-              data: {
-                secret: secret
-              },
-              dataType: 'json'
-            }
-          )
-          .then(function (result) {
-            // Get key from result and set it to output of the task
-            // This key will be stored in Toloka as task result
-            task.setSolutionOutputValues({
-              'output_key': result.key
-            });
+      const task = this;
+      $(task.getDOMElement()).find('.submit').click(function (e) {
+        // Get value from the input field
+        var secret = $(task.getDOMElement()).find('.secret').val();
 
-            // Submit the task after the output is ready
-            // Since the system 'Submit' button is hidden, make this explicitly
-            assignment.submit()
-          });
+        // Call data storage backend
+        $.ajax(
+          {
+            url: `${deployUrl}/result`,
+            type: 'PUT',
+            data: {
+            secret: secret
+            },
+            dataType: 'json'
+          }
+        )
+        .then(function (result) {
+          // Get key from result and set it to output of the task
+          // This key will be stored in Toloka as task result
+          task.setSolutionOutputValue('output_key', result.key);
+
+          delete task.getSolution().output_values['secret'];
+
+          // Submit the task after the output is ready
+          // Since the system 'Submit' button is hidden, make this explicitly
+          assignment.submit()
         });
-      })(this);
+      });
+    },
+
+    onDestroy: function () {
+      $(this.getDOMElement()).find('.submit').off('click');
     }
   });
   
